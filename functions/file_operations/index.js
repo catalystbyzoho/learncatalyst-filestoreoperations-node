9ded9ddb0f8e34f3c547fd5cb7e390728d66b3fe
arungokul5 <92108788+arungokul5@users.noreply.github.com>
2023-07-28 12:44:26 +0530

    updated temp path for file upload

--- functions/file_operations/index.js
+++ functions/file_operations/index.js
@@ -38,7 +38,7 @@ app.post('/uploadFile', async (req, res) => {
 		const app = catalyst.initialize(req);
 		await req.files.data.mv(`/tmp/${req.files.data.name}`);
 		await app.filestore().folder(FOLDER_ID).uploadFile({
-			code: fs.createReadStream(`${__dirname}/uploads/${req.files.data.name}`),
+			code: fs.createReadStream(`/tmp/${req.files.data.name}`),
 			name: req.files.data.name
 		});
 		res.status(200).send({
